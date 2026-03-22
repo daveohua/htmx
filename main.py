3a8c0bc5312f9a74ad16868844a5814f1b835fda
David Ohuabunwa <mail@davidohuabunwa.net>
2026-03-22 03:36:50 +0000

    unfinished: contacts endpoint + html template

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from htmx!")
+from flask import Flask, redirect, request
 
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    main()
+@app.route("/")
+def index():
+    return redirect("/contacts") 
+
+@app.route("/contacts")
+def contacts():
+    search = request.args.get("q")
+    if search is not None:
+        contacts_set = Contact.search(search)
+    else:
+        contacts_set = Contact.all()
+    return render_template("index.html", contacts=contacts_set) 
+
